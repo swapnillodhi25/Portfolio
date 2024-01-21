afc73e769316ec14076930c07c8a0e37cd798b89
Swapnil Lodhi <143304605+swapnillodhi25@users.noreply.github.com>
2024-01-21 23:36:50 +0530

    Update style.js

--- style.js
+++ style.js
@@ -38,7 +38,12 @@ var typed =new Typed(".multiple-text",{
 })
 
 
-
+var content = document.getElementsByTagName('body')[0];
+    var darkMode = document.getElementById('dark-change');
+    darkMode.addEventListener('click', function(){
+        darkMode.classList.toggle('active');
+        content.classList.toggle('night');
+    })
 
 
 
